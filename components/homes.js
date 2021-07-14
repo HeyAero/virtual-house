5116a5f215c137be62b9b9bde314d1de774dd2b2
HeyAero <aeroyts@icloud.com>
2021-07-14 11:43:15 +0100

    update homes data to include location & price, update homes component to show new data

--- components/homes.js
+++ components/homes.js
@@ -1,11 +1,16 @@
 export default function Homes({ homes }) {
   return (
-    <>
+    <div className="flex flex-wrap">
       {homes.map((home) => (
-        <div>
-          <h5>{home.name}</h5>
+        <div className="w-1/3 p-5">
+          <div className="text-center">
+            <h5>{home.name}</h5>
+            <h3>${home.price}</h3>
+            <p>Location: {home.location}</p>
+            <a>View Here</a>
+          </div>
         </div>
       ))}
-    </>
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Footer from '../components/footer'
 import Header from '../components/header'
 import Homes from '../components/homes'
 
-const homes = [{name: 'Home 1'}, {name: 'Home 2'}, {name: 'Home 3'}]
+const homes = [{name: 'Home 1', price: 250000, location: "Texas"}, {name: 'Home 2', price: 550000, location: "New York"}, {name: 'Home 3', location: "Florida"}, {name: 'Home 4', price: 1250000, location: "Texas"}, {name: 'Home 5', price: 5550000, location: "New York"}, {name: 'Home 6', price: 250000, location: "Florida"}]
 
 export default function Home() {
   return (
