5be9c55522b89bf3a469bd0871fdb7e704756608
HeyAero <aeroyts@icloud.com>
2021-07-14 14:12:14 +0100

    remove data from index and add data file with moved data

---
+++ data.js
@@ -0,0 +1,31 @@
+const homes = [
+  {
+    name: 'Home 1',
+    price: 250000,
+    location: "Texas"
+  },
+  {
+    name: 'Home 2',
+    price: 550000,
+    location: "New York"
+  },
+  {
+    name: 'Home 3',
+    location: "Florida"
+  },
+  {
+    name: 'Home 4',
+    price: 1250000,
+    location: "Texas"
+  },
+  {
+    name: 'Home 5',
+    price: 5550000,
+    location: "New York"
+  },
+  {
+    name: 'Home 6',
+    price: 250000,
+    location: "Florida"
+  }
+]
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,6 @@ import Header from '../components/header'
 import Homes from '../components/homes'
 import useSWR from 'swr'
 
-const homes = [{name: 'Home 1', price: 250000, location: "Texas"}, {name: 'Home 2', price: 550000, location: "New York"}, {name: 'Home 3', location: "Florida"}, {name: 'Home 4', price: 1250000, location: "Texas"}, {name: 'Home 5', price: 5550000, location: "New York"}, {name: 'Home 6', price: 250000, location: "Florida"}]
-
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Home() {
