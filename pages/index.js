0edd9259b7af2987a2d7402039a04924d85b88df
HeyAero <aeroyts@icloud.com>
2021-07-14 11:20:37 +0100

    add fake homes base data and edit Homes component to include prop data

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@
 // import styles from '../styles/Home.module.css'
 import Footer from '../components/footer'
 import Header from '../components/header'
+import Homes from '../components/homes'
+
+const homes = [{name: 'Home 1'}, {name: 'Home 2'}, {name: 'Home 3'}]
 
 export default function Home() {
   return (
@@ -12,6 +15,7 @@ export default function Home() {
         <h1 className="text-7xl text-center p-5">Find your dream home.</h1>
         <h5 className="text-2xl text-center p-5">View from our range:</h5>
       </div>
+      <Homes homes={homes}/>
       <Footer/>
     </>
   )
