6b7cdf9b16973148b4375907a319a806e08ba1a2
HeyAero <aeroyts@icloud.com>
2021-07-14 12:00:13 +0100

    add home page for unique homes

---
+++ pages/home/[slug].js
@@ -0,0 +1,15 @@
+import Footer from '../../components/footer'
+import Header from '../../components/header'
+
+export default function Home() {
+  return (
+    <>
+      <Header/>
+      <div className="home-banner bg-purple-300">
+        <h1 className="text-7xl text-center p-5">Find your dream home.</h1>
+        <h5 className="text-2xl text-center p-5">View from our range:</h5>
+      </div>
+      <Footer/>
+    </>
+  )
+}
